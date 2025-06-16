3990ccbb87ec98878b9daff1d25524e5cf748461
Subject-2407 <stevanputra.bintang@gmail.com>
2025-06-16 17:25:06 +0700

    removed useless file

--- src/implementation/model/interfaces/AuthenticableUser.java
+++
@@ -1,8 +0,0 @@
-package implementation.model.interfaces;
-
-import shared.enums.Role;
-
-public interface AuthenticableUser extends User {
-    Role getRole();
-    AuthenticableUser findById(int id);
-}
--- src/implementation/model/interfaces/User.java
+++
@@ -1,7 +0,0 @@
-package implementation.model.interfaces;
-
-// Interface for all type of users
-
-public interface User extends Identifiable {
-    boolean validatePassword(String password);
-}
